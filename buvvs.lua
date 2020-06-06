4d3b0532766b6be640bc299335995ac6556af0e2
TheDoninator <34394614+TheDoninator@users.noreply.github.com>
2020-06-06 15:45:16 -0700

    Add files via upload

--- buvvs.lua
+++ buvvs.lua
@@ -1,51 +1,51 @@
-local f = CreateFrame("Frame", "BasicBuffsFrame", UIParent)
+local BuvvsFrame = CreateFrame("Frame", "Buvvs", UIParent)
 
-local bg = f:CreateTexture()
-bg:SetAllPoints(f)
+local bg = BuvvsFrame:CreateTexture()
+bg:SetAllPoints(BuvvsFrame)
 bg:SetColorTexture(0, 1, 0, 0.3)
 bg:Show()
 
-local SetPoint = f.SetPoint
-local ClearAllPoints = f.ClearAllPoints
+local SetPoint = BuvvsFrame.SetPoint
+local ClearAllPoints = BuvvsFrame.ClearAllPoints
 ClearAllPoints(BuffFrame)
-SetPoint(BuffFrame, "TOPRIGHT", f, "TOPRIGHT")
+SetPoint(BuffFrame, "TOPRIGHT", BuvvsFrame, "TOPRIGHT")
 hooksecurefunc(BuffFrame, "SetPoint", function(frame)
 	ClearAllPoints(frame)
-	SetPoint(frame, "TOPRIGHT", f, "TOPRIGHT")
+	SetPoint(frame, "TOPRIGHT", BuvvsFrame, "TOPRIGHT")
 end)
 
-local header = f:CreateFontString(nil, "OVERLAY", "TextStatusBarText")
-header:SetAllPoints(f)
-header:SetText("BasicBuffs")
+local header = BuvvsFrame:CreateFontString(nil, "OVERLAY", "TextStatusBarText")
+header:SetAllPoints(BuvvsFrame)
+header:SetText("Buvvs")
 header:Show()
 
-f:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
-f:SetWidth(280)
-f:SetHeight(225)
-f:Show()
-f:EnableMouse(true)
-f:RegisterForDrag("LeftButton")
-f:SetMovable(true)
-f:SetScript("OnDragStart", function(frame) frame:StartMoving() end)
-f:SetScript("OnDragStop", function(frame)
+BuvvsFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+BuvvsFrame:SetWidth(280)
+BuvvsFrame:SetHeight(225)
+BuvvsFrame:Show()
+BuvvsFrame:EnableMouse(true)
+BuvvsFrame:RegisterForDrag("LeftButton")
+BuvvsFrame:SetMovable(true)
+BuvvsFrame:SetScript("OnDragStart", function(frame) frame:StartMoving() end)
+BuvvsFrame:SetScript("OnDragStop", function(frame)
 	frame:StopMovingOrSizing()
 	local a, _, b, c, d = frame:GetPoint()
-	BasicBuffsOptions[1] = a
-	BasicBuffsOptions[2] = b
-	BasicBuffsOptions[3] = c
-	BasicBuffsOptions[4] = d
+	BuvvsOptions[1] = a
+	BuvvsOptions[2] = b
+	BuvvsOptions[3] = c
+	BuvvsOptions[4] = d
 end)
 
-f:RegisterEvent("PLAYER_LOGIN")
-f:SetScript("OnEvent", function(display)
-	if not BasicBuffsOptions then
-		BasicBuffsOptions = {"CENTER", "CENTER", 0, 0, false}
+BuvvsFrame:RegisterEvent("PLAYER_LOGIN")
+BuvvsFrame:SetScript("OnEvent", function(display)
+	if not BuvvsOptions then
+		BuvvsOptions = {"CENTER", "CENTER", 0, 0, false}
 	end
 
 	display:ClearAllPoints()
-	display:SetPoint(BasicBuffsOptions[1], UIParent, BasicBuffsOptions[2], BasicBuffsOptions[3], BasicBuffsOptions[4])
+	display:SetPoint(BuvvsOptions[1], UIParent, BuvvsOptions[2], BuvvsOptions[3], BuvvsOptions[4])
 
-	if BasicBuffsOptions[5] then
+	if BuvvsOptions[5] then
 		bg:Hide()
 		header:Hide()
 		display:EnableMouse(false)
@@ -56,23 +56,23 @@ f:SetScript("OnEvent", function(display)
 	display:SetScript("OnEvent", nil)
 end)
 
-SlashCmdList.BASICBUFFS = function()
-	if not BasicBuffsOptions then return end
+SlashCmdList.Buvvs = function()
+	if not BuvvsOptions then return end
 
-	if not BasicBuffsOptions[5] then
+	if not BuvvsOptions[5] then
 		bg:Hide()
 		header:Hide()
-		f:EnableMouse(false)
-		f:SetMovable(false)
-		BasicBuffsOptions[5] = true
-		print("|cFF33FF99BasicBuffs|r:", _G.LOCKED)
+		BuvvsFrame:EnableMouse(false)
+		BuvvsFrame:SetMovable(false)
+		BuvvsOptions[5] = true
+		print("|cFF33FF99Buvvs|r:", _G.LOCKED)
 	else
 		bg:Show()
 		header:Show()
-		f:EnableMouse(true)
-		f:SetMovable(true)
-		BasicBuffsOptions[5] = false
-		print("|cFF33FF99BasicBuffs|r:", _G.UNLOCK)
+		BuvvsFrame:EnableMouse(true)
+		BuvvsFrame:SetMovable(true)
+		BuvvsOptions[5] = false
+		print("|cFF33FF99Buvvs|r:", _G.UNLOCK)
 	end
 end
-SLASH_BASICBUFFS1 = "/basicbuffs"
+SLASH_Buvvs1 = "/Buvvs"
